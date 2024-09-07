eebd9cd3c2c2d23db33b9ce88108106b542b7f08
Dimitri Marion <dimitri.marion@gmail.com>
2024-09-07 18:29:17 +0200

    generate csv

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import axios from "axios";
+import "dotenv/config";
+import { mkConfig, generateCsv, asString } from "export-to-csv";
+import { writeFile } from "node:fs";
+import { Buffer } from "node:buffer";
 
 const args = process.argv.slice(2);
 console.log(args);
@@ -9,7 +13,7 @@ async function fetchWeather(location) {
       "http://api.weatherapi.com/v1/forecast.json",
       {
         params: {
-          key: "c7ddd9a98ae54282bd1142642240709",
+          key: process.env.WEATHER_API_KEY,
           q: location,
           days: 4,
           aqi: "no",
@@ -20,14 +24,21 @@ async function fetchWeather(location) {
 
     const { forecast } = response.data;
     const { forecastday } = forecast;
+    const csvData = [];
 
     forecastday.forEach((forecast) => {
       const { date, day } = forecast;
       const averageTemp = day.avgtemp_c;
       const condition = day.condition.text;
 
+      csvData.push({
+        date,
+        météo: condition,
+        "température moyenne": averageTemp,
+      });
       console.log(`${date}, ${condition}, ${averageTemp}`);
     });
+    return csvData;
   } catch (e) {
     const { code } = e;
     console.log(code);
@@ -35,4 +46,18 @@ async function fetchWeather(location) {
   }
 }
 
-fetchWeather(args[0]);
+const data = await fetchWeather(args[0]);
+console.log(data);
+
+// mkConfig merges your options with the defaults
+// and returns WithDefaults<ConfigOptions>
+const csvConfig = mkConfig({ useKeysAsHeaders: true });
+const csv = generateCsv(csvConfig)(data);
+const filename = `${csvConfig.filename}.csv`;
+const csvBuffer = new Uint8Array(Buffer.from(asString(csv)));
+
+// Write the csv file to disk
+writeFile(filename, csvBuffer, (err) => {
+  if (err) throw err;
+  console.log("file saved: ", filename);
+});
